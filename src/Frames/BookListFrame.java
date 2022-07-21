5badd205c1197163706088f46e9cafd76a85e6f6
Elad Aviv <e.aviv23@gmail.com>
2022-07-21 18:37:53 +0300

    adding dynamic deleting notebooks using checkbox

--- src/Frames/BookListFrame.java
+++ src/Frames/BookListFrame.java
@@ -1,6 +1,5 @@
 package Frames;
 
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
@@ -11,6 +10,7 @@ import java.util.List;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -25,38 +25,43 @@ public class BookListFrame extends JFrame {
 	private JPanel notebookButtonsPanel;
 	private List<BookBase> bookList;
 	private List<JButton> notebookBtnsList;
+	private List<JCheckBox> notebookCheckBoxList;
 	private String bookName;
-	
+	private List<BookBase> selectedNoteBooks;
+
 	public BookListFrame(String bookName) {
 		this.bookName = bookName;
 		this.bookList = FileManager.getAllBookList(bookName);
 		this.notebookBtnsList = new ArrayList<JButton>();
+		this.notebookCheckBoxList = new ArrayList<JCheckBox>();
+		this.selectedNoteBooks = new ArrayList<BookBase>();
 		this.panel = new MenuPanel();
 		this.controlBtnPanel = new JPanel();
 		setSize(370, 400);
 		addControlButtons();
 		addNotebookButtons();
+		// addNotebookCheckBox();
 		panel.setBackground(new Color(150, 255, 255));
 		controlBtnPanel.setBackground(new Color(231, 154, 100));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		notebookButtonsPanel.setOpaque(false);
-		//this.add(notebookButtonsPanel, BorderLayout.NORTH);
-		//this.panel.add(controlBtnPanel);
+		// this.add(notebookButtonsPanel, BorderLayout.NORTH);
+		// this.panel.add(controlBtnPanel);
 		this.add(panel);
 		this.add(controlBtnPanel, BorderLayout.SOUTH);
 		setVisible(true);
 	}
-	
+
 	public void addControlButtons() {
 		final JButton addBtn = new JButton("Add new book");
 		addBtn.setEnabled(true);
-        final JButton removeBtn = new JButton("Remove last");
-        final JButton menuBtn = new JButton("Back to menu");
-        addBtn.addActionListener(new ActionListener() {
-			
-        	/**
-        	 * add new notebook
-        	 */
+		final JButton removeBtn = new JButton("Remove last");
+		final JButton menuBtn = new JButton("Back to menu");
+		addBtn.addActionListener(new ActionListener() {
+
+			/**
+			 * add new notebook
+			 */
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				int index = bookList.size() + 1;
@@ -66,35 +71,56 @@ public class BookListFrame extends JFrame {
 				JButton btn = createNotebookButton(newBook, index);
 				notebookBtnsList.add(btn);
 				panel.add(btn, BorderLayout.CENTER);
+
+				JCheckBox checkBox = createNotebookCheckBox(newBook, index);
+				notebookCheckBoxList.add(checkBox);
+				panel.add(checkBox, BorderLayout.WEST);
+
 				panel.revalidate();
 			}
 		});
-        removeBtn.addActionListener(new ActionListener() {
+		removeBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (bookList.size() >= 2) {
-					FileManager.deleteNotebook(bookList.get(bookList.size() - 1));
-					bookList.remove(bookList.size() - 1);
-					notebookBtnsList.get(notebookBtnsList.size() - 1).hide();
-					notebookBtnsList.remove(notebookBtnsList.size() - 1);
+				if (bookList.size() >= 2 && selectedNoteBooks.size() < bookList.size()) {
+					if (selectedNoteBooks.size() > 0){
+						for (BookBase notebook : selectedNoteBooks) {
+							Integer index = bookList.indexOf(notebook);
+							FileManager.deleteNotebook(bookList.get(index));
+							bookList.remove(notebook);
+							notebookBtnsList.get(index).hide();
+							notebookBtnsList.remove(notebookBtnsList.get(index));
+
+							notebookCheckBoxList.get(index).hide();
+							notebookCheckBoxList.remove(notebookCheckBoxList.get(index));
+						}
+					}
+					selectedNoteBooks.clear();
+					// FileManager.deleteNotebook(bookList.get(bookList.size() - 1));
+					// bookList.remove(bookList.size() - 1);
+					// notebookBtnsList.get(notebookBtnsList.size() - 1).hide();
+					// notebookBtnsList.remove(notebookBtnsList.size() - 1);
+
+					// notebookCheckBoxList.get(notebookCheckBoxList.size() - 1).hide();
+					// notebookCheckBoxList.remove(notebookCheckBoxList.size() - 1);	
 				}
 			}
 		});
-        menuBtn.addActionListener(new ActionListener() {
-			
-        	/**
-        	 * back to menu
-        	 */
+		menuBtn.addActionListener(new ActionListener() {
+
+			/**
+			 * back to menu
+			 */
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+
 				setVisible(false);
 			}
 		});
-        controlBtnPanel.add(addBtn);
-        controlBtnPanel.add(removeBtn);
-        controlBtnPanel.add(menuBtn);
+		controlBtnPanel.add(addBtn);
+		controlBtnPanel.add(removeBtn);
+		controlBtnPanel.add(menuBtn);
 	}
-	
+
 	public JButton createNotebookButton(BookBase bookBase, int index) {
 		JButton btn = new JButton(String.format("Notebook %d", index));
 		btn.setBackground(bookBase.getColor());
@@ -107,7 +133,7 @@ public class BookListFrame extends JFrame {
 		});
 		return btn;
 	}
-	
+
 	/**
 	 * creating the buttons
 	 */
@@ -115,9 +141,51 @@ public class BookListFrame extends JFrame {
 		notebookButtonsPanel = new JPanel();
 		int index = 1;
 		for (BookBase bookBase : bookList) {
-			JButton btn = createNotebookButton(bookBase, index++);
+			JButton btn = createNotebookButton(bookBase, index);
 			panel.add(btn, BorderLayout.CENTER);
 			this.notebookBtnsList.add(btn);
+
+			JCheckBox checkBox = createNotebookCheckBox(bookBase, index);
+			panel.add(checkBox, BorderLayout.WEST);
+			this.notebookCheckBoxList.add(checkBox);
+
+			index++;
 		}
 	}
+
+	/**
+	 * creating the CheckBoxs
+	 */
+	public JCheckBox createNotebookCheckBox(BookBase bookBase, int index) {
+		JCheckBox checkBox = new JCheckBox();
+		checkBox.setBackground(bookBase.getColor());
+		checkBox.setFont(new Font("Arial", Font.PLAIN, 28));
+
+		checkBox.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("index is " + index);
+				System.out.println("is selected " + checkBox.isSelected());
+
+				if (!checkBox.isSelected() && selectedNoteBooks.contains(bookBase)) {
+					System.out.println("in condition one");
+					selectedNoteBooks.remove(selectedNoteBooks.indexOf(bookBase));
+				} else if (checkBox.isSelected() && !selectedNoteBooks.contains(bookBase)) {
+					System.out.println("in condition two");
+					selectedNoteBooks.add(bookBase);
+				}
+			}
+		});
+		return checkBox;
+	}
+
+	// public void addNotebookCheckBox() {
+	// 	// notebookButtonsPanel = new JPanel();
+	// 	int index = 1;
+	// 	for (BookBase bookBase : bookList) {
+	// 		JCheckBox checkBox = createNotebookCheckBox(bookBase, index++);
+	// 		panel.add(checkBox, BorderLayout.EAST);
+	// 		this.notebookCheckBoxList.add(checkBox);
+	// 	}
+	// }
 }
